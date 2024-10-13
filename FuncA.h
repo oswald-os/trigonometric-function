687ad92db33b1b654ab744070aad0d2b339d2c8f
Dmitry Lakin <lakin.dmitry@lll.kpi.ua>
2024-10-13 19:36:08 +0300

    Added an ability to specify function custom terms

--- FuncA.h
+++ FuncA.h
@@ -9,9 +9,33 @@ using namespace std;
 
 class FuncA {
 public:
-    double taylor_sine(double x);
+
+    /**
+     * 
+     * @brief Calculate sin(x) using the Taylor series.
+     * 
+     * This function takes two numbers as input, perform the computing of
+     * sin(x) using the Taylor series, and returns the result.
+     * 
+     * @param x The function x value.
+     * @param terms The function terms.
+     * @return The sin(x) value.
+     * 
+     */
+    double taylor_sine(double x, int terms = 3);
 
 private:
+
+    /**
+     * 
+     * @brief Compute factorial of a number.
+     * 
+     * This function takes one integer as input, calculates its factorial, and
+     * returns the result.
+     * 
+     * @param n The number whose factorial will be calculated.
+     * 
+     */
     unsigned long long factorial(int n);
 };
 
